723dd554bc3c9b125d9f3284de78d8febef2b8fd
Romain Rigaux <romain@cloudera.com>
2018-08-24 12:41:35 +0200

    HUE-8516 [cluster] List more namespaces and filter out invalide ones

--- desktop/core/src/desktop/api2.py
+++ desktop/core/src/desktop/api2.py
@@ -148,7 +148,7 @@ def get_context_computes(request, interface):
           'status': cluster.get('status'),
           'namespace': cluster.get('namespaceCrn', cluster.get('crn')),
           'type': 'altus-adb'
-        } for cluster in AnalyticDbApi(request.user).list_clusters()['clusters'] if cluster.get('status') == 'CREATED']
+        } for cluster in AnalyticDbApi(request.user).list_clusters()['clusters'] if cluster.get('status') == 'CREATED' and cluster.get('cdhVersion') >= 'CDH515']
       )
 
   if interface == 'oozie' or interface == 'jobs' or interface == 'spark2':
